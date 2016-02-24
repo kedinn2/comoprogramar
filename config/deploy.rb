a8fab5a3eb55fa15da256a9f0c083e9be7dd6b36
kedinn2 <kedinn.th@gmail.com>
2016-02-23 21:29:21 -0500

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'comoprogramar.org'
+set :application, 'comoprogramar'
 set :repo_url, 'git@github.com:kedinn2/comoprogramar.git'
 set :deploy_to, '/var/www/comoprogramar.org/html'
 
