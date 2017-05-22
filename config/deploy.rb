58db2e9ceadc4d274c7b5c5f4f1ded3bc3e5a9ed
kedinn2 <kedinn.th@gmail.com>
2017-05-22 00:28:27 -0500

    Up

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,10 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
 
 set :application, 'comoprogramar'
 set :repo_url, 'git@github.com:kedinn2/comoprogramar.git'
-set :deploy_to, '/var/www/comoprogramar.org/html/comoprogramar'
+set :deploy_to, '/var/www/comoprogramar.org/html/comoprogramar/'
 
 
 
@@ -46,15 +43,32 @@ set :repo_tree, '_site'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+set :format, :pretty
+namespace :deploy do
+  task :update_jekyll do
+    on roles(:app) do
+      within "#{deploy_to}/current" do
+      	execute :jekyll, "build"
+      end
     end
   end
 
 end
+
+after "deploy:symlink:release", "deploy:update_jekyll"
+
+
+
+#namespace :deploy do
+
+ # after :restart, :clear_cache do
+ #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+#      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+#    end
+#  end
+
+#end
