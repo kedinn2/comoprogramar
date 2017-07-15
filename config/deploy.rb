b7a72aacbd300b0a95477401c78cd1f8cb16634f
kedinn2 <kedinn.th@gmail.com>
2017-07-15 17:49:30 -0500

    Hii

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,15 +53,15 @@ set :repo_tree, '_site'
 
 
 
-#namespace :deploy do
+namespace :deploy do
 
-#  after :restart, :clear_cache do
-#    on roles(:web), in: :groups, limit: 3, wait: 10 do
-#      # Here we can do anything such as:
-#       within release_path do
-#         execute :rake, 'cache:clear'
-#       end
-#    end
-#  end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+       within release_path do
+         execute :rake, 'cache:clear'
+       end
+    end
+  end
 
-#end
+end
